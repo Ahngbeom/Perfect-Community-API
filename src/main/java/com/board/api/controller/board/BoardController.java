c27cf61dd6b277458837d0ffda764a968bc36ae5
42bahn <bbu0704@gmail.com>
2022-12-20 00:33:10 +0900

    ✅ User authorities verification during the Create Board process

--- src/main/java/com/board/api/controller/board/BoardController.java
+++ src/main/java/com/board/api/controller/board/BoardController.java
@@ -1,10 +1,12 @@
 package com.board.api.controller.board;
 
 import com.board.api.dto.board.BoardDTO;
+import com.board.api.service.auth.AuthService;
 import com.board.api.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,6 +21,8 @@ public class BoardController {
 
     private final Logger log = LogManager.getLogger();
     private final BoardService service;
+
+    private final AuthService authService;
 
     @GetMapping({"/list", ""})
     public ResponseEntity<List<BoardDTO>> getBoardList() {
@@ -40,8 +44,14 @@ public class BoardController {
         try {
             if (principal == null)
                 throw new AuthenticationException("Not logged in.");
+            if (!authService.hasRole(principal.getName(), "admin"))
+                throw new AuthenticationException("Unauthorized.");
+            log.info(principal);
             boardDTO.setCreatedUser(principal.getName());
             return ResponseEntity.ok(service.createBoard(boardDTO));
+        } catch (AuthenticationException authenticationException) {
+            authenticationException.printStackTrace();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(authenticationException.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body(e.getMessage());
