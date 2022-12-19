37c1ecc2031e42b6651b331420c1ae7185e8c11f
42bahn <bbu0704@gmail.com>
2022-12-20 00:39:37 +0900

    ✅ AuthorityDTO implements GrantedAuthority

--- src/main/java/com/board/api/controller/user/AuthenticationController.java
+++ src/main/java/com/board/api/controller/user/AuthenticationController.java
@@ -1,12 +1,12 @@
 package com.board.api.controller.user;
 
-import com.board.api.dto.AuthorityDTO;
+import com.board.api.dto.auth.AuthorityDTO;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.board.api.service.AuthService;
+import com.board.api.service.auth.AuthService;
 
 import java.util.List;
 
