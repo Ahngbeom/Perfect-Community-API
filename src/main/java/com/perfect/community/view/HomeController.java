69cf362a400d54a974ee6743e8c1de3869bda78a
42bahn <bbu0704@gmail.com>
2023-01-03 14:45:44 +0900

    Controller for Views

---
+++ src/main/java/com/perfect/community/view/HomeController.java
@@ -0,0 +1,15 @@
+package com.perfect.community.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public ModelAndView home(ModelAndView mv) {
+        mv.setViewName("api/docs");
+        return mv;
+    }
+}
---
+++ web/WEB-INF/views/api/docs.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: bbu0704
+  Date: 2023-01-03
+  Time: 오후 2:40
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
