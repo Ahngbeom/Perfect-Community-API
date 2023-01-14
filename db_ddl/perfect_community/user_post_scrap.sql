71be561aabcf6aee474818007d43090d6488e46d
42bahn <bbu0704@gmail.com>
2023-01-14 22:54:57 +0900

    ✅ Exported DB Data

--- db_ddl/perfect_community/user_post_scrap.sql
+++ db_ddl/perfect_community/user_post_scrap.sql
@@ -1,7 +1,10 @@
 create table user_post_scrap
 (
-    user_id varchar(20)  not null,
-    post_no int unsigned not null,
+    user_id    varchar(20)                           not null,
+    post_no    int unsigned                          not null,
+    scrap_date timestamp default current_timestamp() not null,
+    constraint user_post_scrap_user_id_post_no_uindex
+        unique (user_id, post_no),
     constraint user_post_scrap_posts_post_no_fk
         foreign key (post_no) references posts (post_no)
             on update cascade on delete cascade,
