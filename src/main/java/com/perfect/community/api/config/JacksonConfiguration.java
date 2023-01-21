fa13f9e50d8e94ef211fa9b548ce4c6b54c2a672
42bahn <bbu0704@gmail.com>
2023-01-21 23:22:00 +0900

    ✅ Register ObjectMapper as bean

---
+++ src/main/java/com/perfect/community/api/config/JacksonConfiguration.java
@@ -0,0 +1,15 @@
+package com.perfect.community.api.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper().registerModule(new JavaTimeModule());
+    }
+}
