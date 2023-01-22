c3f7c697e8dc64341378b142eb7e1c3a7edb3147
42bahn <bbu0704@gmail.com>
2023-01-22 13:33:40 +0900

    ✅ Exported DB Data

--- db_ddl/perfect_community/login_history.sql
+++ db_ddl/perfect_community/login_history.sql
@@ -1,10 +1,10 @@
-create table perfect_community.login_history
+create table login_history
 (
     user_id        varchar(20)                           null,
     ip_address     varchar(50)                           null,
-    user_agent     varchar(100)                          null,
+    user_agent     varchar(200)                          null,
     logged_in_date timestamp default current_timestamp() not null,
     constraint login_history_users_user_id_fk
-        foreign key (user_id) references perfect_community.users (user_id)
+        foreign key (user_id) references users (user_id)
 );
 
--- db_ddl/perfect_community/users.sql
+++ db_ddl/perfect_community/users.sql
@@ -1,4 +1,4 @@
-create table perfect_community.users
+create table users
 (
     user_id     varchar(50)                           not null
         primary key,
