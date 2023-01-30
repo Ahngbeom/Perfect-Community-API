d5042f5f4ef4c699de034d5386408b5f8db04ad9
ahngbeom <bbu0704@gmail.com>
2023-01-30 22:42:41 +0900

    ✅ Exported DB Data

--- db_ddl/perfect_community/user_recently_viewed_posts.sql
+++ db_ddl/perfect_community/user_recently_viewed_posts.sql
@@ -1,6 +1,11 @@
 create table user_recently_viewed_posts
 (
-    user_id varchar(20) null,
+    user_id   varchar(20)                           null,
+    post_no   int unsigned                          not null,
+    view_date timestamp default current_timestamp() not null,
+    constraint user_recently_viewed_posts_posts_null_fk
+        foreign key (post_no) references posts (post_no)
+            on update cascade on delete cascade,
     constraint user_recently_viewed_posts_users_user_id_fk
         foreign key (user_id) references users (user_id)
             on update cascade on delete cascade
