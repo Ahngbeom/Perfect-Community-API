8acee11ebd4ca34ff62f0a5e84db9ace0fe2f030
42bahn <bbu0704@gmail.com>
2023-02-01 19:58:42 +0900

    ✅ Reorganized JUnit tests

--- src/test/java/com/perfect/community/api/mapper/post/PostInteractionMapperTest.java
+++ src/test/java/com/perfect/community/api/mapper/post/PostInteractionMapperTest.java
@@ -2,11 +2,41 @@ package com.perfect.community.api.mapper.post;
 
 import com.perfect.community.api.vo.post.PostRecommendVO;
 import com.perfect.community.api.vo.post.PostViewsVO;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.annotation.Rollback;
 
-@Rollback(value = false)
-public class InteractionTest extends PostMapperTest {
+@DisplayName("Board Interaction Test")
+class PostInteractionMapperTest extends PostMapperTest {
+
+    @Test
+    @DisplayName("Initialize views")
+    void initializeViews() {
+        log.info(postInteractionMapper.initializeViews(1));
+    }
+
+    @Test
+    @DisplayName("Initialize recommend and not recommend")
+    void initializeRecommend() {
+        log.info(postInteractionMapper.initializeRecommend(1));
+    }
+
+    @Test
+    @DisplayName("Get views")
+    void getViews() {
+        log.info(postInteractionMapper.getViews(1));
+    }
+
+    @Test
+    @DisplayName("Get recommended")
+    void getRecommend() {
+        log.info(postInteractionMapper.getRecommend(1));
+    }
+
+    @Test
+    @DisplayName("Get not recommended")
+    void getNotRecommend() {
+        log.info(postInteractionMapper.getNotRecommend(1));
+    }
 
     @Test
     void increaseViews() {
